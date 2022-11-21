103e3036970cf25f73918722592ef9ab4b195cc2
IlfiDev <ilyafillipov2@gmail.com>
2022-11-22 00:21:20 +0300

    changes to cook

--- Cook.cs
+++ Cook.cs
@@ -1,15 +1,26 @@
 class Cook{
-    private String _order;
-    private String _preparedOrder;
+
+    private OrderQueue _queue;
+    private String _order = "";
+    private String _preparedOrder = "";
+
+    public Cook(OrderQueue queue){
+        _queue = queue;
+    }
+
     public void GetOrder(string order){
-        if(_order != null){
-            this._order = order;
+        if(_order != ""){
+            _order = _queue.GetOrder();
         }
     }
 
     private void cook(){
+        Thread.Sleep(300);
+
         _preparedOrder = _order;
-        _order = null
+        _order = "";
+        _queue.AddPreparedOrder(_preparedOrder);
+
     }
 
 }
