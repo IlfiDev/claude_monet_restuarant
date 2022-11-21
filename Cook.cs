8841c23ea8bacbabe2b7dabe30fc53165b041b60
IlfiDev <ilyafillipov2@gmail.com>
2022-11-21 22:02:59 +0300

    Added orders queue

--- Cook.cs
+++ Cook.cs
@@ -1,3 +1,15 @@
 class Cook{
-    
+    private String _order;
+    private String _preparedOrder;
+    public void GetOrder(string order){
+        if(_order != null){
+            this._order = order;
+        }
+    }
+
+    private void cook(){
+        _preparedOrder = _order;
+        _order = null
+    }
+
 }
