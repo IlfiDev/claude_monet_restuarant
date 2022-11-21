e056e3c8db785a4b12ebeb2f6984075fa01c8f0f
Koviello Arlekkino <vainglory.ru@mail.ru>
2022-11-22 01:25:03 +0300

    add methods

--- Waiter.cs
+++ Waiter.cs
@@ -1,3 +1,31 @@
-class Waiter{
+using System.Collections.Generic;
 
+class Waiter{
+    public OrderQueue orders;
+    public List<Table> readyToOrderTable;
+    public string order;
+    public Waiter(List<Table> tables, OrderQueue _orders)
+    {
+        readyToOrderTable = tables;
+        orders = _orders;
+    }
+
+    public void TakeOrder(){
+        foreach (Table table in readyToOrderTable)
+        {
+            order = table.GetVisitor().GetOrder();
+            orders.AddOrder(order);
+        }
+    }
+
+    public void GiveOrder(){
+        foreach (Table table in readyToOrderTable)
+        {
+            order = orders.GetPreparedOrder();
+
+            if (table.Equals(order)){
+                table.GetVisitor().GiveOrder(order);
+            }
+        }
+    }
 }
