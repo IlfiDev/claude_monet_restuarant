17e7c98a6408e7fc77f761689c16f84009b5b570
Ovik7 <melkon-ovik@mail.ru>
2022-11-27 18:25:23 +0300

    АЙНУРА

--- Ainura.cs
+++ Ainura.cs
@@ -1,3 +1,16 @@
+using System.Collections.Generic;
+
 class Ainura{
-    
+    private List<Table> _tables;
+    private void CheckTables(){
+        foreach (Table table in _tables)
+        {
+            
+        }
+    }
+    public void CleanUp(string clean){
+        if(_tables != clean){
+            this._tables = clean;
+        }
+    }
 }
